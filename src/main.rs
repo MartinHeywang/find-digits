e2b57a0604bdbd3eb294b8c7b9bec89c3e03eecb
Martin Heywang <martin.heywang1@gmail.com>
2022-11-03 11:09:24 +0100

    Now using BigUint (and modpow) for speeding the process up

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::io;
+extern crate num;
+use num::{BigUint, One, Zero};
+
+// don't know what this line does but looks mandatory
+use std::{io, ops::{Div, Rem, AddAssign}};
+
+fn from(val: u64) -> BigUint {
+    BigUint::from(val)
+}
 
 fn main() {
     // program configuration
-    let base: u64 = 13;
-    let first: u64 = 7;
+    let base = from(13u64);
+    let first  = from(7u64);
 
     println!("Enter n: (and press ENTER)");
-    let second: u64 = number_input();
+    let second = from(number_input());
 
     println!("Enter NB: (and press ENTER)");
     let nb_digits: u32 = number_input().try_into().unwrap();
@@ -15,16 +23,16 @@ fn main() {
     println!("Trying to find {nb_digits} digits of {base}^({first}^{second}).");
     println!("");
 
-    let exponent_last_digits = find_digits(first, second, nb_digits);
+    let exponent_last_digits = find_digits(&first, &second, nb_digits);
     println!("|1| {first}^{second} = 10^{nb_digits}*d + {exponent_last_digits}");
 
-    let base_congruent = find_congruent_to_one(base, nb_digits);
+    let base_congruent = find_congruent_to_one(&base, nb_digits);
     println!("|2| {base}^{base_congruent} === 1 [10^{nb_digits}]");
 
     let u_remainder = exponent_last_digits % base_congruent;
     println!("|3| calculating : {base}^{u_remainder} = ? (finding {nb_digits} digits)");
 
-    let result: u64 = find_digits(base, u_remainder, nb_digits);
+    let result = find_digits(&base, &u_remainder, nb_digits);
 
     println!("|4| {base}^({first}^{second}) = ...{result}");
     println!("(-> info: si le résultat n'a pas le bon nombre de chiffre, il faut ajouter des zéros devant.)");
@@ -32,68 +40,36 @@ fn main() {
 
 }
 
-fn find_digits(base: u64, exponent: u64, nb_digits: u32) -> u64 {
-
-    let modulo: u64 = u64::pow(10, nb_digits);
-    let mut current: u64 = 1;
-
-    // we use (modulo - 1) as the initialization value
-    // because it is a power of 10 -> the biggest number possible with nb_digits digits
-    let mut current_factor: u64 = modulo - 1;
-
-    let mut factor_idx: u64 = 0;
-    let factor_idx = loop {
-        match current_factor.checked_mul(base) {
-            Some(num) => current_factor = num,
-            None => break factor_idx,
-        }
-        factor_idx += 1;
-    };
-
-    let factor = base.pow((factor_idx).try_into().unwrap());
-
-    // maybe consider handling overflows with a checked_mul and applying the modulo only after overflows ?
-    // and exit the infinite loop to do the last multiplications
-
-    let exponent_quotient = exponent.div_euclid(factor_idx);
-    let exponent_remainder = exponent.rem_euclid(factor_idx);
-
-    for _k in 1..=exponent_quotient {
-        current = current * factor % modulo;
-    }
-
-    for _ in 1..=exponent_remainder {
-        current = current * base % modulo;
-    }
-
-    return current;
+fn find_digits(base: &BigUint, exponent: &BigUint, nb_digits: u32) -> BigUint {
+    let modulo = BigUint::pow(&from(10u64), nb_digits);
+    return base.modpow(exponent, &modulo);
 }
 
-fn find_congruent_to_one(base: u64, nb_digits: u32) -> u64 {
+fn find_congruent_to_one(base: &BigUint, nb_digits: u32) -> BigUint {
 
-    let modulo: u64 = u64::pow(10, nb_digits);
+    let modulo = BigUint::pow(&from(10u64), nb_digits);
 
     // special cases
-    if base == 13 && modulo % 10 == 0 {
+    if base == &from(13u64) && (&modulo).rem(from(10u64)) == from(0u64) {
         // this is only based on something that we noticed through the runs
         // and this saves precious seconds
-        return if modulo == 10 {
-            4
-        } else if modulo == 100 {
-            20
-        } else if modulo == 1000 {
-            100
+        return if modulo == from(10u64) {
+            from(4u64)
+        } else if modulo == from(100u64) {
+            from(20u64)
+        } else if modulo == from(1000u64) {
+            from(100u64)
         } else {
-            modulo / 20
+            modulo.div(from(20u64))
         };
     }
 
-    let mut remainder: u64 = 1;
-    let mut k: u64 = 0;
+    let mut remainder = from(1u64);
+    let mut k = from(0u64);
 
-    while remainder != 1 || k == 0 {
-        remainder = remainder * base % modulo;
-        k += 1;
+    while One::is_one(&remainder) || Zero::is_zero(&k) {
+        remainder = &remainder * base % &modulo;
+        BigUint::add_assign(&mut k, &One::one());
     }
 
     return k;
